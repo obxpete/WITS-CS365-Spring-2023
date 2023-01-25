65a7405ad2fd7f9f1023d4c6bdcc02b774aacc23
Peter Lombardo <obxpete@users.noreply.github.com>
2023-01-25 14:03:45 +0000

    adds database and basic inserts

--- resources/sql/tasktable.sql
+++ resources/sql/tasktable.sql
@@ -1,6 +1,14 @@
 -- SQLite
-CREATE TABLE TASKS (
+
+
+
+CREATE TABLE  IF NOT EXISTS TASKS (
   ID INTEGER NOT NULL,
   taskName TEXT NOT NULL,
   description TEXT
-)
+);
+
+INSERT INTO TASKS (ID, taskName, description) VALUES (1, 'Task 1', 'Description 1');
+INSERT INTO TASKS (ID, taskName, description) (ID, taskName, description) VALUES (2, 'Task 2', 'Description 2');
+INSERT INTO TASKS (ID, taskName, description) (ID, taskName, description) VALUES (3, 'Task 3', 'Description 3');
+INSERT INTO TASKS (ID, taskName, description) (ID, taskName, description) VALUES (4, 'Task 4', 'Description 4');
