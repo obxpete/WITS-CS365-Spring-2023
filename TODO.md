d7d8bd62a5bcee7cedd2f8fbb6c0f9e48f12deb1
Peter Lombardo <obxpete@users.noreply.github.com>
2023-04-18 23:21:30 +0000

    refactors to module - stubbed

--- TODO.md
+++ TODO.md
@@ -1,7 +1,6 @@
 
 run export PS1='${PWD/*\//} > '
 
-
 steps:
 - Web site to display the results 
 - Refactor to use a NodeJS Module 
--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
+// const helloClass = require('./modules/helloClass')
+
 const app = express();
 
 app.get('/', function(request, response) {
-
-    response.send('Hello World!'); // add <h1> tag.  then change to function call helloClass()
+    response.send(helloClass()); // add helloClass.
 })
 
 var port = process.env.PORT ||  8090;
@@ -11,10 +12,3 @@ app.listen(port);
 console.log('API is running at + http://localhost:' + port)
 
 
-
-// function helloClass() {
-//     let classNames = ["Devorah Leah Caplan", "Adina Ehrlich", "Shira Ely", "Yehudis Wenger", "Tzippora Weiskopf", "Leora Samuels","Zahava Rosenbaum", "Rochel Neuberger", "Shoshana Linzer", "Rechel Langer", "Yael Kibel", "Chava Glickstein"];
-    
-//     return  classNames.join(' and <br> ');
-    
-// }
