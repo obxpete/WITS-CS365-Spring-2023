312fbdcec122f23d3afefc3f12cbcfdefc31d1ad
Peter Lombardo <obxpete@users.noreply.github.com>
2023-04-18 22:31:51 +0000

    adds function

--- app.js
+++ app.js
@@ -2,9 +2,19 @@ const express = require('express');
 const app = express();
 
 app.get('/', function(request, response) {
-    response.send('Hello from my  app');
+
+    response.send('Hello World!'); // add <h1> tag.  then change to function call helloClass()
 })
 
 var port = process.env.PORT ||  8090;
 app.listen(port);
 console.log('API is running at + http://localhost:' + port)
+
+
+
+// function helloClass() {
+//     let classNames = ["Devorah Leah Caplan", "Adina Ehrlich", "Shira Ely", "Yehudis Wenger", "Tzippora Weiskopf", "Leora Samuels","Zahava Rosenbaum", "Rochel Neuberger", "Shoshana Linzer", "Rechel Langer", "Yael Kibel", "Chava Glickstein"];
+    
+//     return  classNames.join(' and <br> ');
+    
+// }
